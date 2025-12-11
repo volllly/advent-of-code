e8ac4ae3ab86dc52472d10c0497baabe8b759f19
Paul Volavsek <paul@volavsek.com>
2025-12-11 18:02:52 +0100

    refactor

--- src/Days/D02.hs
+++ src/Days/D02.hs
@@ -1,54 +1,46 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module Days.D02 (part1, part2) where
 
-import Data.Function ((&))
-import Data.List (List, scanl', unfoldr)
-import Debug.Trace (traceShow)
+import Data.List.Split (chunksOf)
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
-data Range a = Range
-  { from :: a,
-    to :: a
+data Range = Range
+  { rangeFrom :: Int,
+    rangeTo :: Int
   }
   deriving (Show)
 
-rangeParser :: Parser (Range Int)
+rangeParser :: Parser Range
 rangeParser = do
   from <- read <$> many1 digit
   _ <- char '-'
   to <- read <$> many1 digit
   return $ Range from to
 
-rangesParser :: Parser (List (Range Int))
-rangesParser = do
-  rangeParser `sepBy1` char ','
+rangesParser :: Parser [Range]
+rangesParser = rangeParser `sepBy1` char ','
 
-parseInput :: String -> Either ParseError (List (Range Int))
+parseInput :: String -> Either ParseError [Range]
 parseInput = parse rangesParser ""
-
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf n = unfoldr (\xs -> if null xs then Nothing else Just (splitAt n xs))
-
-divisors :: Int -> [Int]
-divisors n = [d | d <- [1 .. n], n `mod` d == 0]
 
 allEqualSplits :: (Int -> [Int]) -> [a] -> [[[a]]]
 allEqualSplits divisors xs = [chunksOf d xs | d <- divisors (length xs)]
 
-part :: (Int -> [Int]) -> String -> Int
-part divisors input =
-  parseInput input
-    & either (error . show) id
-    >>= (\r -> [from r .. to r])
-    & filter
-      ( \id ->
-          let splits = allEqualSplits divisors (show $ id)
-           in if null splits then False else splits & any (\split -> split & all (== head split))
-      )
-    & sum
+allChunksEqual :: (Eq a) => [a] -> Bool
+allChunksEqual [] = True
+allChunksEqual (x : xs) = all (== x) xs
+
+solve :: (Int -> [Int]) -> String -> Int
+solve divisors input =
+  let ranges = either (error . show) id $ parseInput input
+      nums = concatMap (\r -> [rangeFrom r .. rangeTo r]) ranges
+      isValid n = any allChunksEqual $ allEqualSplits divisors $ show n
+   in sum $ filter isValid nums
 
 part1 :: String -> Int
-part1 = part (\n -> if even n then [n `div` 2] else [])
+part1 = solve (\n -> [n `div` 2 | n > 1, even n])
 
 part2 :: String -> Int
-part2 = part (\n -> divisors n & filter (\d -> d /= n))
+part2 = solve (\n -> [d | d <- [1 .. n - 1], n `mod` d == 0])
